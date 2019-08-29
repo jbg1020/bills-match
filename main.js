ad69a0d200b13519d84ced12956b436691131edb
jbg1020 <jbg1020@gmail.com>
2019-08-28 20:07:07 -0700

    got card to show, working on dynamic shuffle function

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
 $(document).ready(initializeApp);
+
+var cardClickOne = null;
+var cardClickTwo = null;
+var uMatched = 0;
+var maxMatched = 9;
+var uAttempts = 0;
+var uGamesPlayed = 0;
+// var cardArray = ['1', '2', '3', '4'];
+// var cardArray = [1,2,3,4];
+// var cardDiv = $('.card');
+
+function initializeApp() {
+    displayShuffledCards();
+}
+
+function cardClickHandler(event) {
+    // $(this).find(.back).addClass("hidden");
+    // conditionals
+}
+
+function displayShuffledCards() {
+    var cardArray = ['1', '2', '3', '4', '1', '2', '3', '4'];
+    for (var i = cardArray.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = cardArray[i];
+        cardArray[i] = cardArray[j];
+        cardArray[j] = temp;
+    }
+    singleClassFromShuffle(cardArray);
+}
+
+function singleClassFromShuffle(array) {
+    var cardDiv = $('.card');
+    for (var arrIndex = 0; arrIndex < array.length; arrIndex++) {
+        cardDiv.addClass(array[arrIndex]);
+        console.log("single item?:", array[arrIndex]);
+    }
+}
