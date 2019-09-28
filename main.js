5f637a3c094226aa195dc9839d255a87b50b7db5
jbg1020 <jbg1020@gmail.com>
2019-09-27 17:22:55 -0700

    win-modal pops up on win, cannot close it yet

--- main.js
+++ main.js
@@ -35,6 +35,20 @@ function renderCardDivs() { // creates divs in random order from shuffleCards fu
     }
 }
 
+function winModal() {
+
+var wooHoo = `<div class="modal-content">
+                <span class="close">&times;</span>
+                <h2>YOU WON</h2>
+              </div>`;
+              $(wooHoo).appendTo(".modal");
+              $('.modal').show();
+
+
+
+
+}
+
 function cardClickHandler(event) {
     if ($(event.target).hasClass('front'))
         return;
@@ -57,6 +71,8 @@ function cardClickHandler(event) {
                 if (uMatched === maxMatched) {
                     uGamesPlayed++
                     console.log("You Won!");
+                    winModal();
+                    // execute winModal
                 }
             } else {
                 canClickMouse = false;
@@ -66,7 +82,7 @@ function cardClickHandler(event) {
             uAttempts++
             displayStats();
             console.log('attempts #:', uAttempts);
-            console.log('accuracy:', calculateAccuracy()+'%');
+            console.log('accuracy:', calculateAccuracy() + '%');
             console.log('event.delegateTarget:', event.delegateTarget)
         }
     }
@@ -81,12 +97,12 @@ function flipBackMismatch() {
     canClickMouse = true;
 }
 
-function calculateAccuracy () {
-    return Math.round(uMatched/uAttempts*100);
+function calculateAccuracy() {
+    return Math.round(uMatched / uAttempts * 100);
 }
 
-function displayStats () {
+function displayStats() {
     $('#num-games-played').text(uGamesPlayed);
     $('#num-attempts').text(uAttempts);
-    $('#pct-accurate').text(calculateAccuracy() +"%");
+    $('#pct-accurate').text(calculateAccuracy() + "%");
 }
